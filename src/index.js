e08d2970aff79f53ea58672a755043cb73af3af8
Rasha Abdulrazzak <rasha.abdulrazzak@gmail.com>
2020-02-01 00:52:52 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -14,3 +14,43 @@ document.addEventListener("DOMContentLoaded", ()=>{
   })
 
 })
+function getResponse(){
+  let configObj = {
+    method: "GET",
+    headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+    },
+    body: ''
+  };
+   
+  fetch("http://localhost:3000/dogs", configObj)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(object) {
+      let div = document.getElementById('toy-collection');
+      let divT = document.createElement('div');
+      let h = document.createElement(h2);
+      let img = document.createElement(img);
+      let p= document.createElement('p');
+      let btn = document.createElement('button');
+      divT.className = 'card';
+      btn.classList.add("like-btn");
+    
+      for(toy of object)
+      {
+         h.innerHTML = toy.name;          
+         img.src = toy.image ;          
+         p.innerHTML= toy.likes;          
+         btn.innerHTML= `like + ${toy.likes}`;
+         divT.prepend(h);
+         h.after(img);
+         img.after(p);
+         p.after(btn);
+         div.appendChild(divT);         
+        }
+    });
+
+}
+getResponse();
